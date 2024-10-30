709c9d5e15e5d40a03b4d8960e1d9cc80c8d78dc
ELMURODSALOMOV <salomovelmurod062@gmail.com>
2024-10-30 12:10:17 +0500

    BROKERS: Impliment Storage Broker

---
+++ PhoneBookConsole/Brokers/Loggings/ILoggingBroker.cs
@@ -0,0 +1,9 @@
+﻿namespace PhoneBookConsole.Brokers.LoggingBroker
+{
+    internal interface ILoggingBroker
+    {
+        void LogInformation(string message);
+        void LogError(string userMessage);
+        void LogError(Exception exception);
+    }
+}
